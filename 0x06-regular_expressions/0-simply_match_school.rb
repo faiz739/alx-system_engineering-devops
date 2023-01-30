7722b62a4612b29b8f5b29f44d4be150b9aa9876
unknown <faithfulchekwas@gmail.com>
2023-01-30 14:47:05 -0800

    all task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
-puts ARGV[0].scan(/School/).join
+
+def match(string)
+  # Define the regular expression to match "School"
+  pattern = /School/
+  
+  # Use the `=~` operator to match the string with the pattern
+  match_data = string =~ pattern
+
+  # Check if there was a match
+  if match_data
+    puts "Match found at index #{match_data}"
+  else
+    puts "No match found"
+  end
+end
+
+# Get the argument from the command line
+argument = ARGV[0]
+
+# Call the match method with the argument
+match(argument)
+
